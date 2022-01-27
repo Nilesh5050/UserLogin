1310d18fb678516d94ce55ea123b2a364c1bc769
Nilesh Vishwakarma <iamnilesh2020@gmail.com>
2022-01-27 12:03:08 +0530

    Add project files.

---
+++ UserLogin/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserLogin.Models;
+
+namespace UserLogin.Controllers
+{
+    public class UserController : Controller
+    {
+        private ApplicationDbContext dbContext;
+
+        public UserController(ApplicationDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public IActionResult Index()
+        {
+            //var customers = dbContext.customers.ToList();
+            var en = dbContext.AspNetUsers.ToList();
+            return View(en);
+        }
+        
+    }
+}
---
+++ UserLogin/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserLogin.Models
+{
+    public class User
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
